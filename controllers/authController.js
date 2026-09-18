80a20f534c874fc4d380b4df29a9018122a8b95e
hyazhar <uddinmdazhar21@gmail.com>
2026-09-18 20:51:34 +0530

    Login Controller and Routes has been implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userSchema');
-const bycrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const ExpressError = require('../utils/ExpressError');
 
 module.exports.register= async (req,res)=>{
@@ -7,6 +8,7 @@ module.exports.register= async (req,res)=>{
     if(!name || !email || !password){
         throw new ExpressError(400, "Name, email and password are required");
     }
+    
     const existingUser= await User.findOne({email});
     if (existingUser){
         throw new ExpressError(409,"User already Exists");
@@ -21,6 +23,50 @@ module.exports.register= async (req,res)=>{
     res.status(201).json({
         success:true,
         message:"User Registered successfully",
+        user:{
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role,
+        },
+    });
+};
+
+module.exports.login = async (req,res)=>{
+    const {email,password}= req.body;
+    
+    if(!email || !password){
+        throw new ExpressError(400, "Email and password are required");
+    }
+    const user = await User.findOne({
+        email:email.toLowerCase(),
+    });
+    
+    if(!user){
+        throw new ExpressError(401,"Invalid email or password");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password,user.password);
+
+    if(!isPasswordCorrect){
+        throw new ExpressError(401,"Invalid email and Password");
+    }
+
+    const token = jwt.sign(
+        {
+            userId: user._id,
+            role : user.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"7d",
+        }
+    );
+
+    res.status(200).json({
+        success:true,
+        message:"Login Successful",
+        token,
         user:{
             id:user._id,
             name:user.name,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 const wrapAsync = require('../utils/wrapAsync');
 
 router.post('/register', wrapAsync(authController.register));
+router.post('/login',wrapAsync(authController.login));
 
 
 
