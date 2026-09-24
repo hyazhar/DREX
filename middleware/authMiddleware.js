57f501d9dce2238788a1a69070b4028b111cb9c7
hyazhar <uddinmdazhar21@gmail.com>
2026-09-24 15:01:11 +0530

    Fixed Bugs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,35 +1,33 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userSchema');
-const ExpressError = require('../utils/ExpressError');
+const jwt = require("jsonwebtoken");
+const User = require("../models/userSchema");
+const ExpressError = require("../utils/ExpressError");
 
-const authMiddleware = async(req,res,next)=>{
-    
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        throw new ExpressError(401,"Authentication required");
-    }
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    throw new ExpressError(401, "Authentication required");
+  }
 
-    if(!authHeader.startsWith("Bearer ")){
-        throw new ExpressError(401,"Invalid authentication format");
+  if (!authHeader.startsWith("Bearer ")) {
+    throw new ExpressError(401, "Invalid authentication format");
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    throw new ExpressError(401, "Authenticatio token is missing");
+  }
+  try {
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password")
+    if(!user){
+        throw new ExpressError(401,"user no longer exists");
     }
-    const token = authHeader.split(" ")[1];
-    if(!token){
-        throw new ExpressError(401,"Authenticatio token is missing");
+    req.user=user;
+    next();
+  } catch (error) {
+    if (error instanceof ExpressError) {
+      throw error;
     }
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId).select("-password")
-        if(!user){
-            throw new ExpressError(401,"user no longer exists");
-        }
-        req.user=user;
-        next();
-    }
-    catch(error){
-        if(error instanceof ExpressError){
-            throw error;
-        }
-        throw new ExpressError(401,"invalid or expired token");
-    }
+    throw new ExpressError(401, "invalid or expired token");
+  }
 };
-module.exports= authMiddleware;
+module.exports = authMiddleware;
